5204f9ac95894ca5007d64a8a030e79dadec346d
sander <sander@muliphen.nl>
2024-11-26 09:47:40 +0100

    Add minimal reproduction

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', //Remove this once we migrate all pages to dark mode
+  content: [],
+  prefix: 'tw-',
+}
+
